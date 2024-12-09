bf9736dc1ab0d446b2b0beb090a6ea6c537f6aec
Anuradha <anuradha.uds@gmail.com>
2024-12-09 08:00:29 +0530

    changed filename

--- tasks-service/src/routes/userRoutes.ts
+++ tasks-service/src/routes/userRoutes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 
-import { getUsers } from "../controllers/externalAPIController";
+import { getUsers } from "../controllers/ExternalAPIController";
 
 const router = Router();
 
