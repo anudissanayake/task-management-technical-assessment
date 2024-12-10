c125021be7aa9c07ef8fe039a024c7f1b7221ba8
Anuradha <anuradha.uds@gmail.com>
2024-12-10 17:45:47 +0530

    server js updated to support both local and lambda

--- tasks-service/src/server.ts
+++ tasks-service/src/server.ts
@@ -5,6 +5,7 @@ import userRoutes from './routes/UserRoutes';
 import errorHandler from './infrastructure/middlewares/ErrorHandler';
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/tasks', taskRoutes);
@@ -12,4 +13,9 @@ app.use('/users', userRoutes);
 
 app.use(errorHandler);  // Error handling middleware
 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+// For the AWS lambda handler
 export default app;
