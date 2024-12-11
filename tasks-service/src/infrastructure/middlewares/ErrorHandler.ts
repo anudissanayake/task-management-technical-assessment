4d6664ec81e8dad7ccbf82a8d15a41dcf99c1680
Anuradha <anuradha.uds@gmail.com>
2024-12-11 23:30:46 +0530

    fixed file-naming

--- tasks-service/src/infrastructure/middlewares/ErrorHandler.ts
+++ tasks-service/src/infrastructure/middlewares/ErrorHandler.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
   res.status(500).json({ message: err.message });
 };
